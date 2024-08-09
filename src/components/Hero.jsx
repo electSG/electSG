f37f1ec3c83e914cc1b0fae7b0a7983aeb33816b
junji101 <junji101@LAPTOP-OH88IVGG>
2024-08-09 13:29:55 +0800

    updated hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <img src={Background} className='w-full h-full object-cover' />
         <div className='max-w-[1140px] m-auto'>
             <div className='absolute top-[40%] w-full md:[40%] max-w-[600px] h-full flex flex-col text-white p-8'>
-                <h2 className='font-bold text-4xl'>Simple. Safe. Secure.Test</h2>
+                <h2 className='font-bold text-4xl'>Simple. Safe. Secure.</h2>
             </div>
         </div>`
     </div>
